fb79d715280bbf8f857f06b9028b3e6bf1e2d3bc
Simon Frezard <simon.frezard@gmail.com>
2020-12-15 10:37:58 +0100

    animation css en vueJs

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const app = new Vue({
     el:"#app",
     data() {
         return {
-            title: "My vue 2.6 app"
+            title: "My vue 2.6 app",
+            text: "Ceci est du texte",
+            verif: true,
+            display: 0
+        }
+    },
+    methods: {
+        animate() {
+            this.display = !this.display;
         }
     }
 });
